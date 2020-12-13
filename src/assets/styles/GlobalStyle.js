155b6590374462835d5c1f6fa4226e99a6af289c
michalsidzej <38423054+michalsidzej@users.noreply.github.com>
2020-12-13 18:57:41 +0100

    Update GlobalStyle.js

--- src/assets/styles/GlobalStyle.js
+++ src/assets/styles/GlobalStyle.js
@@ -10,7 +10,6 @@ const GlobalStyle = createGlobalStyle`
   }
   *, *::before, *::after {
     box-sizing: border-box;
-    -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
 
