32c7deec6fb9e64a0bd6204afd66ac10294a8142
michalsidzej <michalsidzej@gmail.com>
2021-01-18 13:29:43 +0100

    removed discount

--- src/pages/sklep.js
+++ src/pages/sklep.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import MainTemplate from 'templates/MainTemplate/MainTemplate';
 import ShopTemplate from 'templates/ShopTemplate/ShopTemplate';
-import PresaleBanner from 'components/PresaleBanner/PresaleBanner';
+// import PresaleBanner from 'components/PresaleBanner/PresaleBanner';
 
 const StyledContainer = styled.div`
   padding: 4rem 2rem;
@@ -18,18 +18,25 @@ const StyledContainer = styled.div`
 `;
 const StyledTitle = styled.h1`
   margin-top: 2rem;
-`
+`;
 const StyledDescription = styled.p`
   margin: 4rem 2rem;
   text-align: justify;
-`
+`;
 
 const Shop = () => (
   <MainTemplate>
-    <PresaleBanner /> 
+    {/* <PresaleBanner />  */}
     <StyledContainer>
       <StyledTitle>Kajaki</StyledTitle>
-      <StyledDescription>Prowadzimy sprzedaż kajaków renomowanych marek EXO oraz Spade. Odpowiedni dobór kształtu kajaka jest jedną z najważniejszych decyzji, kiedy zaczynamy przygodę z kajakarstwem. W razie pytań odnośnie doboru kajaka, służymy pomocą. Zadzwoń na <a href='tel:+48695283124'>+48 695 283 124</a> lub napisz na <a href='mailto:info@terrakajaki.pl'>info@terrakajaki.pl</a></StyledDescription>
+      <StyledDescription>
+        Prowadzimy sprzedaż kajaków renomowanych marek EXO oraz Spade.
+        Odpowiedni dobór kształtu kajaka jest jedną z najważniejszych decyzji,
+        kiedy zaczynamy przygodę z kajakarstwem. W razie pytań odnośnie doboru
+        kajaka, służymy pomocą. Zadzwoń na{' '}
+        <a href="tel:+48695283124">+48 695 283 124</a> lub napisz na{' '}
+        <a href="mailto:info@terrakajaki.pl">info@terrakajaki.pl</a>
+      </StyledDescription>
       <ShopTemplate />
     </StyledContainer>
   </MainTemplate>
